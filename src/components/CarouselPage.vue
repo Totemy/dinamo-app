3ebd603431b0bb119e4ba5e02eb9071959000733
Totemy <thetotemofficial@gmail.com>
2024-03-10 22:08:32 +0200

    Edited carousel

--- src/components/CarouselPage.vue
+++ src/components/CarouselPage.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="wiper-wrapper">
     <div class="wiper">
-      <button class="wiper-button wiper-button__left" @click="prevSlide" :class="{ 'is-hidden': currentIndex === 0 }">
+      <button class="wiper-button wiper-button__left" @click="nextSlide" :class="{ 'is-hidden': currentIndex === slides.length - 1 }">
         <img src="/img/carousel/left-arrow.svg" alt="Prev" />
       </button>
 
-      <ul class="wiper-track" :style="{ transform: `translateX(-${currentIndex * (200)}px)` }">
+      <ul class="wiper-track" :style="{ transform: `translateX(-${currentIndex * (293)}px)` }">
         <li v-for="(slide, index) in slides" :key="index" class="wiper-item"
             :class="{ 'active-swipe': index === currentIndex }">
-          <img :src="slide.image" class="wiper__image">
+          <img :src="slide.image" class="wiper__image" alt="slider-image">
+          <p class="wiper__text"> {{slide.text}}</p>
         </li>
       </ul>
 
-      <button class="wiper-button wiper-button__right" @click="nextSlide" :class="{ 'is-hidden': currentIndex === slides.length - 1 }">
+      <button class="wiper-button wiper-button__right" @click="prevSlide" :class="{ 'is-hidden': currentIndex === 0 }">
         <img src="/img/carousel/left-arrow.svg" alt="Next" />
       </button>
     </div>
@@ -26,12 +27,12 @@ export default {
       currentIndex: 0,
       itemWidth: 0,
       slides: [
-        { image: '/img/carousel/1.jpg'},
-        { image: '/img/carousel/2.jpg'},
-        { image: '/img/carousel/3.jpg'},
-        { image: '/img/carousel/4.jpg'},
-        { image: '/img/carousel/5.jpg'},
-        { image: '/img/carousel/6.jpg'},
+        { image: '/img/carousel/1.jpg', text: 'Салаты'},
+        { image: '/img/carousel/2.jpg', text: 'Выпечка'},
+        { image: '/img/carousel/3.jpg', text: 'Супы'},
+        { image: '/img/carousel/4.jpg', text: 'Блюда на мангале'},
+        { image: '/img/carousel/5.jpg', text: 'Горячие блюда'},
+        { image: '/img/carousel/6.jpg', text: 'Соусы'},
       ]
     };
   },
