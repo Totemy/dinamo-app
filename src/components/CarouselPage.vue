9a0026c906997e93fca27cfb7c75cf68550983dd
Totemy <thetotemofficial@gmail.com>
2024-03-12 11:54:51 +0200

    Edited carousel final

--- src/components/CarouselPage.vue
+++ src/components/CarouselPage.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="wiper-wrapper">
     <div class="wiper">
-      <button class="wiper-button wiper-button__left" @click="nextSlide" :class="{ 'is-hidden': currentIndex === slides.length - 1 }">
+      <button class="wiper-button wiper-button__left" @click="nextSlide">
         <img src="/img/carousel/left-arrow.svg" alt="Prev" />
       </button>
 
-      <ul class="wiper-track" :style="{ transform: `translateX(-${currentIndex * (293)}px)` }">
-        <li v-for="(slide, index) in slides" :key="index" class="wiper-item"
+      <ul class="wiper__track" :style="{ transform: `translateX(-${currentIndex * (293)}px)` }">
+        <li v-for="(slide, index) in slides" :key="index" class="wiper__item "
             :class="{ 'active-swipe': index === currentIndex }">
-          <img :src="slide.image" class="wiper__image" alt="slider-image">
-          <p class="wiper__text"> {{slide.text}}</p>
+            <img :src="slide.image" class="wiper__image" alt="slider-image">
+            <p class="wiper__text"> {{slide.text}}</p>
+            <button v-if="index === currentIndex" class="btn btn__open-menu">Открыть меню</button>
+          <div v-else class="overlay"></div>
         </li>
       </ul>
-
-      <button class="wiper-button wiper-button__right" @click="prevSlide" :class="{ 'is-hidden': currentIndex === 0 }">
+      <button class="wiper-button wiper-button__right" @click="prevSlide" >
         <img src="/img/carousel/left-arrow.svg" alt="Next" />
       </button>
     </div>
