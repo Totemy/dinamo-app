d949a629ead49f0ddfb7ccdcb12eea1185da1500
Totemy <thetotemofficial@gmail.com>
2024-03-28 13:34:20 +0200

    Add navbar text

--- src/views/HeaderView.vue
+++ src/views/HeaderView.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="header">
     <div class="container">
-      <div>
-        <router-link to="/">Logo</router-link>
+      <div class="navbar-web">
+        <div class="navbar-web__row">
+          <div>Logo</div>
+          <div>главная</div>
+          <div>меню</div>
+          <div>доставка</div>
+          <div>контакты</div>
+          <div>акции</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+</script>
