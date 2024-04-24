1c1da9b7f4acf928c8ceaf55a7c6517c5521e124
Totemy <thetotemofficial@gmail.com>
2024-04-24 22:30:35 +0300

    Added content to navbar

--- src/views/HeaderView.vue
+++ src/views/HeaderView.vue
@@ -3,19 +3,27 @@
     <div class="container">
       <div class="navbar-web">
         <div class="navbar-web__row">
-          <div>Logo</div>
-          <div>главная</div>
-          <div>меню</div>
-          <div>доставка</div>
-          <div>контакты</div>
-          <div>акции</div>
+          <div class="navbar-web__item"> <img src="/img/footer/logo.png"> </div>
+          <div class="navbar-web__item">главная</div>
+          <div class="navbar-web__item">меню</div>
+          <div class="navbar-web__item">доставка</div>
+          <div class="navbar-web__item">контакты</div>
+          <div class="navbar-web__item">акции</div>
+          <div class="navbar-web__phone">
+            +380 (98) 765 43 21
+          </div>
+          <div class="navbar-web__social">
+            <div><img class="navbar-web__social_img" src="/img/header/instagram.png" /></div>
+            <div><img class="navbar-web__social_img" src="/img/header/facebook.png" /></div>
+            <div><img class="navbar-web__social_img" src="/img/header/twitter.png" /></div>
+          </div>
         </div>
       </div>
       <div class="navbar-mobile">
-        <div>
-          <span></span>
-          <span></span>
-          <span></span>
+        <div class="navbar-burger">
+          <span class="navbar-burger__bar"></span>
+          <span class="navbar-burger__bar"></span>
+          <span class="navbar-burger__bar"></span>
         </div>
         <div>
           <div>Logo</div>
