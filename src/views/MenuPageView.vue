9cdd69953c1b8eaa9e48b45d186cd5154122dc95
Totemy <thetotemofficial@gmail.com>
2024-05-02 23:35:21 +0300

    Edited styles for menu

--- src/views/MenuPageView.vue
+++ src/views/MenuPageView.vue
@@ -1,5 +1,6 @@
 <template>
-  <section>
+  <section class="menu-page">
+    <div class="container">
     <div class="menu-page__gallery">
       Gallery
     </div>
@@ -7,6 +8,24 @@
       <h2>Холодные закуски</h2>
     </div>
     <div class="menu-page__blocks">
+      <div class="menu-page__block">
+        <div class="menu-page__header">
+          <div class="menu-page__title">Сациви из курицы</div>
+          <div class="menu-page__price">390 грн</div>
+        </div>
+        <div class="menu-page__info">
+          <div class="menu-page__size">200гр.</div>
+          <div class="menu-page__size">КБЖУ: 320/16/27/4</div>
+        </div>
+        <div>
+          <div class="menu-page__composition">
+            Состав: кур. грудка 100г; орех грец. 60г; вода 20г; шафран 5г; уксус винный 1,5 ст.л.; масло слив.
+            2ст.л.; соль,перец по вкусу; хмели сунели 1ч.л.;чеснок 5г; лук репчатый 10г.
+
+          </div>
+        </div>
+
+      </div>
       <div class="menu-page__block">
         <div class="menu-page__header">
           <div class="menu-page__title">Сациви из курицы</div>
@@ -25,18 +44,74 @@
         <div class="menu-page__price">390 грн</div>
       </div>
       <div class="menu-page__block">
-        <div>
-          <div class="menu-page__title"></div>
-          <div class="menu-page__price"></div>
+        <div class="menu-page__header">
+          <div class="menu-page__title">Сациви из курицы</div>
+        </div>
+        <div class="menu-page__info">
+          <div class="menu-page__size">200гр.</div>
+          <div class="menu-page__size">КБЖУ: 320/16/27/4</div>
         </div>
         <div>
-          <div class="menu-page__size"></div>
-          <div class="menu-page__size"></div>
+          <div class="menu-page__composition">
+            Состав: кур. грудка 100г; орех грец. 60г; вода 20г; шафран 5г; уксус винный 1,5 ст.л.; масло слив.
+            2ст.л.; соль,перец по вкусу; хмели сунели 1ч.л.;чеснок 5г; лук репчатый 10г.
+
+          </div>
         </div>
-        <div>
-          <div class="menu-page__info"></div>
-        </div>
+        <div class="menu-page__price">390 грн</div>
       </div>
+      <div class="menu-page__block">
+        <div class="menu-page__header">
+          <div class="menu-page__title">Сациви из курицы</div>
+        </div>
+        <div class="menu-page__info">
+          <div class="menu-page__size">200гр.</div>
+          <div class="menu-page__size">КБЖУ: 320/16/27/4</div>
+        </div>
+        <div>
+          <div class="menu-page__composition">
+            Состав: кур. грудка 100г; орех грец. 60г; вода 20г; шафран 5г; уксус винный 1,5 ст.л.; масло слив.
+            2ст.л.; соль,перец по вкусу; хмели сунели 1ч.л.;чеснок 5г; лук репчатый 10г.
+
+          </div>
+        </div>
+        <div class="menu-page__price">390 грн</div>
+      </div>
+      <div class="menu-page__block">
+        <div class="menu-page__header">
+          <div class="menu-page__title">Сациви из курицы</div>
+        </div>
+        <div class="menu-page__info">
+          <div class="menu-page__size">200гр.</div>
+          <div class="menu-page__size">КБЖУ: 320/16/27/4</div>
+        </div>
+        <div>
+          <div class="menu-page__composition">
+            Состав: кур. грудка 100г; орех грец. 60г; вода 20г; шафран 5г; уксус винный 1,5 ст.л.; масло слив.
+            2ст.л.; соль,перец по вкусу; хмели сунели 1ч.л.;чеснок 5г; лук репчатый 10г.
+
+          </div>
+        </div>
+        <div class="menu-page__price">390 грн</div>
+      </div>
+      <div class="menu-page__block">
+        <div class="menu-page__header">
+          <div class="menu-page__title">Сациви из курицы</div>
+        </div>
+        <div class="menu-page__info">
+          <div class="menu-page__size">200гр.</div>
+          <div class="menu-page__size">КБЖУ: 320/16/27/4</div>
+        </div>
+        <div>
+          <div class="menu-page__composition">
+            Состав: кур. грудка 100г; орех грец. 60г; вода 20г; шафран 5г; уксус винный 1,5 ст.л.; масло слив.
+            2ст.л.; соль,перец по вкусу; хмели сунели 1ч.л.;чеснок 5г; лук репчатый 10г.
+
+          </div>
+        </div>
+        <div class="menu-page__price">390 грн</div>
+      </div>
+    </div>
     </div>
   </section>
 </template>
